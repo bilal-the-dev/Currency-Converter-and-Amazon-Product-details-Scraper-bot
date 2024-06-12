cee82aeca9e11b0fdae8a8f5ec5317e0a043d12e
Bilal <bilalfarooq6001@gmail.com>
2024-06-12 09:59:47 +0500

    debug

--- src/commands/scraping/amazon.js
+++ src/commands/scraping/amazon.js
@@ -1,109 +1,124 @@
 const {
-  ApplicationCommandOptionType,
-  PermissionFlagsBits,
-  EmbedBuilder,
+	ApplicationCommandOptionType,
+	PermissionFlagsBits,
+	EmbedBuilder,
 } = require("discord.js");
 const randomUseragent = require("random-useragent");
 
 module.exports = {
-  name: "amazon",
-  description: "checks from the amazon price product",
-  options: [
-    {
-      name: "link",
-      description: "the link of amazon product",
-      required: true,
-      type: ApplicationCommandOptionType.String,
-    },
-  ],
-  callback: async (client, interaction) => {
-    let page;
-    try {
-      await interaction.deferReply();
-      const url = interaction.options.get("link").value;
+	name: "amazon",
+	description: "checks from the amazon price product",
+	options: [
+		{
+			name: "link",
+			description: "the link of amazon product",
+			required: true,
+			type: ApplicationCommandOptionType.String,
+		},
+	],
+	callback: async (client, interaction) => {
+		let page;
+		try {
+			await interaction.deferReply();
+			const url = interaction.options.get("link").value;
 
-      if (!isAmazonLink(url))
-        return await interaction.editReply("Only Amazon links are allowed");
-      page = await client.browser.newPage();
+			if (!isAmazonLink(url))
+				return await interaction.editReply("Only Amazon links are allowed");
+			page = await client.browser.newPage();
 
-      await page.setViewport({ width: 1280, height: 720 });
+			await page.setViewport({ width: 1280, height: 720 });
 
-      const userAgent = randomUseragent.getRandom();
-      const UA = userAgent;
+			const userAgent = randomUseragent.getRandom();
+			const UA = userAgent;
 
-      await page.setUserAgent(UA);
+			await page.setUserAgent(UA);
 
-      await page.goto(url);
-      await page.screenshot({
-        path: "demo.png",
-      });
+			await page.goto(url);
+			await page.screenshot({
+				path: "demo.png",
+			});
+			const fs = require("fs");
+			const path = require("path");
 
-      const productDetails = await page.evaluate(() => {
-        let title = document.querySelector("#productTitle")?.innerText.trim();
+			const pageHTML = await page.content();
 
-        if (!title) title = document.querySelector("#title")?.innerText.trim();
+			// Define the output file path
+			// const filePath = path.resolve(__dirname, 'page.js');
 
-        const price = document
-          .querySelector(".a-price .a-offscreen")
-          ?.innerText.trim();
+			// Write the HTML content to the file
+			fs.writeFile("page.js", pageHTML, (err) => {
+				if (err) {
+					console.error("Error writing to file", err);
+				} else {
+					console.log("HTML content saved to page.html");
+				}
+			});
 
-        const rating = document.querySelector(".a-icon-alt")?.innerText.trim();
+			const productDetails = await page.evaluate(() => {
+				let title = document.querySelector("#productTitle")?.innerText.trim();
 
-        let image = document.querySelector("#imgTagWrapperId img")?.src;
+				if (!title) title = document.querySelector("#title")?.innerText.trim();
 
-        if (!image)
-          image = document.querySelector("#landing-image-wrapper img")?.src;
-        return {
-          title,
-          price,
-          rating,
-          image,
-        };
-      });
-console.log(productDetails)
-      // const TEST_DATA = {
-      //   title: "test title",
-      //   price: "$100",
-      //   rating: "5",
-      //   image: "ddd",
-      // };
+				const price = document
+					.querySelector(".a-price .a-offscreen")
+					?.innerText.trim();
 
-      if (!productDetails["price"])
-        return await interaction.editReply(
-          "Something went wrong with fetching the product details, please try again."
-        );
+				const rating = document.querySelector(".a-icon-alt")?.innerText.trim();
 
-      const discountedPrice = applyDiscount(productDetails["price"]);
+				let image = document.querySelector("#imgTagWrapperId img")?.src;
 
-      const embed = new EmbedBuilder()
-        .setColor("#0099ff")
-        .setTitle(productDetails.title ?? 'Data found')
-        .setDescription(
-          `**Original Price**: ${productDetails.price}\n**Discounted Price**: ${discountedPrice}\n**Rating**: ${productDetails.rating}`
-        )
-        .setTimestamp();
+				if (!image)
+					image = document.querySelector("#landing-image-wrapper img")?.src;
+				return {
+					title,
+					price,
+					rating,
+					image,
+				};
+			});
+			console.log(productDetails);
+			// const TEST_DATA = {
+			//   title: "test title",
+			//   price: "$100",
+			//   rating: "5",
+			//   image: "ddd",
+			// };
 
-      productDetails.image && embed.setThumbnail(productDetails.image)
+			if (!productDetails["price"])
+				return await interaction.editReply(
+					"Something went wrong with fetching the product details, please try again."
+				);
 
+			const discountedPrice = applyDiscount(productDetails["price"]);
 
-      await interaction.editReply({ embeds: [embed] });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      if (page && !page.isClosed()) await page.close();
-    }
-  },
+			const embed = new EmbedBuilder()
+				.setColor("#0099ff")
+				.setTitle(productDetails.title ?? "Data found")
+				.setDescription(
+					`**Original Price**: ${productDetails.price}\n**Discounted Price**: ${discountedPrice}\n**Rating**: ${productDetails.rating}`
+				)
+				.setTimestamp();
+
+			productDetails.image && embed.setThumbnail(productDetails.image);
+
+			await interaction.editReply({ embeds: [embed] });
+		} catch (error) {
+			console.log(error);
+		} finally {
+			if (page && !page.isClosed()) await page.close();
+		}
+	},
 };
 
 function isAmazonLink(url) {
-  const amazonPattern = /^(https?:\/\/)?(www\.)?amazon\.[a-z\.]{2,6}\/.*$/i;
-  return amazonPattern.test(url);
+	const amazonPattern = /^(https?:\/\/)?(www\.)?amazon\.[a-z\.]{2,6}\/.*$/i;
+	return amazonPattern.test(url);
 }
 
 function applyDiscount(priceString) {
-  const priceWithoutDollar = priceString.replace("$", "");
-  const price = parseFloat(priceWithoutDollar);
-  const discountedPrice = price * 0.25;
-  const formattedPrice = `$${discountedPrice.toFixed(2)}`;
-  return formattedPrice;
+	const priceWithoutDollar = priceString.replace("$", "");
+	const price = parseFloat(priceWithoutDollar);
+	const discountedPrice = price * 0.25;
+	const formattedPrice = `$${discountedPrice.toFixed(2)}`;
+	return formattedPrice;
 }
