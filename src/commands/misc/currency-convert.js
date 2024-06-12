e7ef84d53ac8ae9c6b5e1b3fa94d109fd4ae2c2f
Bilal <bilalfarooq6001@gmail.com>
2024-06-12 19:57:42 +0500

    make commands staff only

--- src/commands/misc/currency-convert.js
+++ src/commands/misc/currency-convert.js
@@ -1,85 +1,88 @@
 const {
-  ApplicationCommandOptionType,
-  PermissionFlagsBits,
-  EmbedBuilder,
-  time,
+	ApplicationCommandOptionType,
+	PermissionFlagsBits,
+	EmbedBuilder,
+	time,
 } = require("discord.js");
 const convertCurrency = require("../../utils/currencyConvertor");
 
 module.exports = {
-  name: "convert",
-  description: "convert a currecncy!",
-  options: [
-    {
-      name: "amount",
-      description: "Amount to Convert.",
-      required: true,
-      type: ApplicationCommandOptionType.Number,
-    },
-    {
-      name: "from-currency",
-      description:
-        "Currency from you want to convert, make sure to add Abbreviation of Currency e.g USD, EUR",
-      required: false,
-      type: ApplicationCommandOptionType.String,
-      choices: [
-        { name: "USD - United States Dollar", value: "USD" },
-        { name: "EUR - Euro", value: "EUR" },
-        { name: "GBP - British Pound Sterling", value: "GBP" },
-      ],
-    },
-    {
-      name: "to-currency",
-      description:
-        "The currency to convert into, make sure to add Abbreviation of Currency e.g USD, EUR",
-      required: false,
-      type: ApplicationCommandOptionType.String,
-      choices: [
-        { name: "USD - United States Dollar", value: "USD" },
-        { name: "EUR - Euro", value: "EUR" },
-        { name: "GBP - British Pound Sterling", value: "GBP" },
-      ],
-    },
-  ],
-  callback: async (client, interaction) => {
-    try {
-      await interaction.deferReply();
-      let fromCurrency =
-        interaction.options.get("from-currency")?.value || "USD";
-      let toCurrency = interaction.options.get("to-currency")?.value || "GBP";
-      const amount = interaction.options.get("amount").value;
+	name: "convert",
+	description: "convert a currecncy!",
+	options: [
+		{
+			name: "amount",
+			description: "Amount to Convert.",
+			required: true,
+			type: ApplicationCommandOptionType.Number,
+		},
+		{
+			name: "from-currency",
+			description:
+				"Currency from you want to convert, make sure to add Abbreviation of Currency e.g USD, EUR",
+			required: false,
+			type: ApplicationCommandOptionType.String,
+			choices: [
+				{ name: "USD - United States Dollar", value: "USD" },
+				{ name: "EUR - Euro", value: "EUR" },
+				{ name: "GBP - British Pound Sterling", value: "GBP" },
+			],
+		},
+		{
+			name: "to-currency",
+			description:
+				"The currency to convert into, make sure to add Abbreviation of Currency e.g USD, EUR",
+			required: false,
+			type: ApplicationCommandOptionType.String,
+			choices: [
+				{ name: "USD - United States Dollar", value: "USD" },
+				{ name: "EUR - Euro", value: "EUR" },
+				{ name: "GBP - British Pound Sterling", value: "GBP" },
+			],
+		},
+	],
+	callback: async (client, interaction) => {
+		try {
+			await interaction.deferReply();
+			if (!interaction.member.roles.cache.has(process.env.STAFF_ROLE_ID))
+				return await interaction.editReply("Staff only.");
 
-      const conversion = await convertCurrency(
-        amount,
-        fromCurrency.toUpperCase(),
-        toCurrency.toUpperCase(),
-        interaction
-      );
-      if (!conversion) {
-        return await interaction.editReply(
-          `Unable to convert from ${fromCurrency} to ${toCurrency}, the entered currency is not correct please make sure to visit https://www.foreignexchangelive.com/currency-codes-symbols/ and  see the corresponding currency code`
-        );
-      }
-      const embed = new EmbedBuilder()
-        .setColor(0x0099ff) // Blue color
-        .setTitle("Currency Conversion")
-        .setDescription(conversion.message)
-        .addFields(
-          {
-            name: "Amount",
-            value: `${conversion.amount} ${conversion.fromCurrency}`,
-            inline: true,
-          },
-          {
-            name: "Converted Amount",
-            value: `${conversion.convertedAmount} ${conversion.toCurrency}`,
-            inline: true,
-          }
-        );
+			let fromCurrency =
+				interaction.options.get("from-currency")?.value || "USD";
+			let toCurrency = interaction.options.get("to-currency")?.value || "GBP";
+			const amount = interaction.options.get("amount").value;
 
-      await interaction.editReply({ embeds: [embed] });
-    } catch (error) {
-      console.log(error);
-    }
-  },
+			const conversion = await convertCurrency(
+				amount,
+				fromCurrency.toUpperCase(),
+				toCurrency.toUpperCase(),
+				interaction
+			);
+			if (!conversion) {
+				return await interaction.editReply(
+					`Unable to convert from ${fromCurrency} to ${toCurrency}, the entered currency is not correct please make sure to visit https://www.foreignexchangelive.com/currency-codes-symbols/ and  see the corresponding currency code`
+				);
+			}
+			const embed = new EmbedBuilder()
+				.setColor(0x0099ff) // Blue color
+				.setTitle("Currency Conversion")
+				.setDescription(conversion.message)
+				.addFields(
+					{
+						name: "Amount",
+						value: `${conversion.amount} ${conversion.fromCurrency}`,
+						inline: true,
+					},
+					{
+						name: "Converted Amount",
+						value: `${conversion.convertedAmount} ${conversion.toCurrency}`,
+						inline: true,
+					}
+				);
+
+			await interaction.editReply({ embeds: [embed] });
+		} catch (error) {
+			console.log(error);
+		}
+	},
 };
--- src/commands/misc/reminder.js
+++ src/commands/misc/reminder.js
@@ -1,47 +1,51 @@
 const {
-  ApplicationCommandOptionType,
-  PermissionFlagsBits,
-  EmbedBuilder,
+	ApplicationCommandOptionType,
+	PermissionFlagsBits,
+	EmbedBuilder,
 } = require("discord.js");
 const data = require("../../../config.json");
 module.exports = {
-  name: "reminder",
-  description: "reminder a member!!!",
-  options: [
-    {
-      name: "target-user",
-      description: "The user to give the reminder.",
-      required: true,
-      type: ApplicationCommandOptionType.User,
-    },
-  ],
-  callback: async (client, interaction) => {
-    await interaction.deferReply();
-    const user = interaction.options.getUser("target-user");
-    if (!user)
-      return await interaction.reply("User does not exist in the server.");
-    const reminderMessage = `Reminder <@${user.id}> your reply is needed in the <#${interaction.channelId}> as soon as possible`;
-    const serverIcon = interaction.guild.iconURL();
-    const userIcon = user.displayAvatarURL();
+	name: "reminder",
+	description: "reminder a member!!!",
+	options: [
+		{
+			name: "target-user",
+			description: "The user to give the reminder.",
+			required: true,
+			type: ApplicationCommandOptionType.User,
+		},
+	],
+	callback: async (client, interaction) => {
+		await interaction.deferReply();
+		const user = interaction.options.getUser("target-user");
+		if (!user)
+			return await interaction.editReply("User does not exist in the server.");
 
-    const reminderEmbed = new EmbedBuilder()
-      .setColor(0x0099ff) // Blue color
-      // .setTitle("Reminder")
-      .setDescription(reminderMessage)
-      .setThumbnail(serverIcon)
-      .setAuthor({
-        name: user.tag,
-        iconURL: userIcon,
-      });
+		if (!interaction.member.roles.cache.has(process.env.STAFF_ROLE_ID))
+			return await interaction.editReply("Staff only.");
 
-    try {
-      await user.send({ embeds: [reminderEmbed] });
-      await interaction.editReply(`Message sent to ${user.tag}`);
-    } catch (error) {
-      console.error("Error sending DM:", error);
-      await interaction.editReply(
-        "Failed to send the message, Bot could not dm the user because he has not allowed to be send the message"
-      );
-    }
-  },
+		const reminderMessage = `Reminder <@${user.id}> your reply is needed in the <#${interaction.channelId}> as soon as possible`;
+		const serverIcon = interaction.guild.iconURL();
+		const userIcon = user.displayAvatarURL();
+
+		const reminderEmbed = new EmbedBuilder()
+			.setColor(0x0099ff) // Blue color
+			// .setTitle("Reminder")
+			.setDescription(reminderMessage)
+			.setThumbnail(serverIcon)
+			.setAuthor({
+				name: user.tag,
+				iconURL: userIcon,
+			});
+
+		try {
+			await user.send({ embeds: [reminderEmbed] });
+			await interaction.editReply(`Message sent to ${user.tag}`);
+		} catch (error) {
+			console.error("Error sending DM:", error);
+			await interaction.editReply(
+				"Failed to send the message, Bot could not dm the user because he has not allowed to be send the message"
+			);
+		}
+	},
 };
